fc2c2bf670e56216c34b3c6e24f2c817a9961dd2
Frederik Hermans <frederik.hermans@hotmail.com>
2018-11-08 20:04:18 +0100

    feat: redux for settings completed

--- components/AgeSelectorComponent.js
+++ components/AgeSelectorComponent.js
@@ -2,36 +2,53 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Slider from "react-native-slider";
 import { Image } from "react-native";
+import { changeSettings } from '../redux/actions/actions';
+import { connect } from 'react-redux';
 
 class AgeSelector extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 18
+      value: this.props.settings.age
     };
+    console.log(this.value);
   }
 
-  ageSelection = value => {};
+  _ageSelection = (value) => {
+    this.setState({ value: value })
+    const userSettings = this.props.settings;
+    userSettings.age= value;
+    this.props.changeSettings(userSettings);
+  };
 
   render() {
     return (
       <View style={styles.container}>
           <Slider
             style = {styles.slidercontainer}
-            value={this.state.value}
+            value={this.state.value ? this.state.value : 18}
             onValueChange={value => this.setState({ value: value })}
-            onSlidingComplete={value => this.ageSelection(value)}
+            onSlidingComplete={value => this._ageSelection(value)}
             maximumValue={99}
             minimumValue={18}
             step={1}
             thumbStyle={styles.thumb}
           />
-           <Text>{this.state.value} years old</Text>
+           <Text>{this.state.value ? this.state.value : 18} years old</Text>
       </View>
     );
   }
 }
-export default AgeSelector;
+
+const mapStateToProps = (state) => ({
+  settings: state.user.settings 
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeSettings: (settings) => dispatch(changeSettings(settings))
+});
+
+export default connect(mapStateToProps,mapDispatchToProps) (AgeSelector);
 
 const styles = StyleSheet.create({
   thumb: {
